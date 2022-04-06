a95163bb89cc9ba80ef3a09c0076fdbddd4bdd57
galo <67323030+gfalotico@users.noreply.github.com>
2022-04-06 17:11:16 -0300

    updates

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,8 @@ import {
   Group,
   Box,
   Button,
+  Paper,
+  Text,
 } from '@mantine/core';
 import Issues from '../components/Issues';
 export default function HomePage() {
@@ -33,8 +35,66 @@ export default function HomePage() {
       })}
     >
       <Container>
+        <Text align='center'>ROOM ID: #dawddad2</Text>
         <Center style={{ width: '100%', height: 800 }}>
-          <div>All elements inside Center are centered</div>
+        <Box
+            sx={{
+              position: 'absolute',
+              transform: `translate(0px,-140px)`,
+            }}
+          >
+            <Paper shadow="xl" style={{ width: 50, height: 100 }} component={Center} radius="lg">1</Paper>
+            <Text align='center'>names</Text>
+          </Box>
+          <Box
+            sx={{
+              position: 'absolute',
+              transform: `translate(100px,-140px)`,
+            }}
+          >
+            <Paper shadow="xl" style={{ width: 50, height: 100 }} component={Center} radius="lg">5</Paper>
+            <Text align='center'>names</Text>
+          </Box>
+          <Box
+            sx={{
+              position: 'absolute',
+              transform: `translate(-100px,-140px)`,
+            }}
+          >
+            <Paper shadow="xl" style={{ width: 50, height: 100 }} component={Center} radius="lg">5</Paper>
+            <Text align='center'>names</Text>
+          </Box>
+          <Box>
+            <Paper shadow="xl" p="xl" style={{ width: 300, height: 100 }} component={Center} radius="xl"></Paper>
+          </Box>
+          <Box
+            sx={{
+              position: 'absolute',
+              transform: `translate(0px,140px)`,
+            }}
+          >
+            <Paper shadow="xl" style={{ width: 50, height: 100 }} component={Center} radius="lg">2</Paper>
+            <Text align='center'>names</Text>
+          </Box>
+          <Box
+            sx={{
+              position: 'absolute',
+              transform: `translate(100px,140px)`,
+            }}
+          >
+            <Paper shadow="xl" component={Center} style={{ width: 50, height: 100 }} radius="lg">9</Paper>
+            <Text align='center'>names</Text>
+          </Box>
+          <Box
+            sx={{
+              position: 'absolute',
+              transform: `translate(-100px,140px)`,
+            }}
+          
+          >
+            <Paper component={Center} shadow="xl" style={{ width: 50, height: 100 }} radius="lg">1</Paper>
+            <Text align='center'>names</Text>
+          </Box>
         </Center>
         <Group position="center">
           {fibonacci.map((item, index) => (
